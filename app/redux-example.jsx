5dd76ea3c02a216ec45b5a2a15e57730719d907c
Rod <rumbalumba89@gmail.com>
2018-11-30 23:25:46 +0000

    unfinished redux lesson 1

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,37 @@
 var redux = require('redux');
 
 console.log(redux);
+
+// Pure function
+// same output using the same input no matter what
+// takes inputs, then returns an output
+// does not change the value of the inputs
+// no side-effects
+function add(a,b) {
+    return a + b;
+};
+
+// impure functions
+
+var a = 3;
+function add(b) {
+    return a + b;
+}
+
+var result;
+function add(a,b) {
+    result = a + b;
+    return result;
+}
+
+function add(a,b) {
+    return a + b + new Date().getSeconds();
+}
+
+var changeProp(obj) {
+    
+}
+
+var res = changeProp({
+    name: 'Jun'
+});
