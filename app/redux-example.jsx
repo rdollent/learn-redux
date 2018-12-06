74bdeb4700985f46cce5278f6ec01102176e5199
Rod <rumbalumba89@gmail.com>
2018-12-06 22:02:54 +0000

    init commit

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -28,10 +28,25 @@ function add(a,b) {
     return a + b + new Date().getSeconds();
 }
 
-var changeProp(obj) {
-    
+function changeProp(obj) {
+    // instead of obj.name = 'Jen';
+    // return obj
+    // that is impure.
+    // objects and arrays are passed by reference, thus changing the value of original data
+    // we want immutability
+    return {
+        ...obj,
+        name: 'Jen'
+    };
 }
 
-var res = changeProp({
-    name: 'Jun'
-});
+
+
+var startingValue = {
+     name: 'Jun',
+     age: 29
+};
+
+var res = changeProp(startingValue);
+
+console.log(startingValue, res);
